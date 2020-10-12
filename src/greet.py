76b485d41e9b443515859477ff0a5fbaba868038
Duleeka Gunatilake <duleekag@gmail.com>
2020-10-12 15:54:50 +0530

    Add mock greet API.

---
+++ src/greet.py
@@ -0,0 +1,6 @@
+
+def greet (name):
+	print("Hello " + name)
+
+def print_version():
+    print("1.0.1")
---
+++ src/greet_test.py
@@ -0,0 +1,6 @@
+import greet
+
+if __name__ == "__main__":
+    print("running main function")
+    greet.greet("John Doe")
+    greet.print_version()
